83ec3996befaaf4ef8e2ee2b1bf93fbac09badd8
Kelvin Kamau <njengak993@gmail.com>
2026-06-14 18:01:02 +0300

    installed pg

--- src/config/database.ts
+++ src/config/database.ts
@@ -7,7 +7,9 @@ export default ({ env }) => ({
       database: env('DATABASE_NAME', 'Agrodakk-db'),
       user: env('DATABASE_USERNAME', 'postgres'),
       password: env('DATABASE_PASSWORD', ''),
-      ssl: env.bool('DATABASE_SSL', false),
+      ssl: env.bool('DATABASE_SSL', true)
+        ? { rejectUnauthorized: false }
+        : false,
     },
   },
 });
